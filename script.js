469521868d47b3b55b7c3a632d310ae12a34e5d5
Saten <249590842+satenshii@users.noreply.github.com>
2026-01-07 19:41:22 -0500

    feat: create function to get random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}

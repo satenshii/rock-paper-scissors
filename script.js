37cb8de0d19c0861b1f07cd5d90f0abb3ac0a466
Saten <249590842+satenshii@users.noreply.github.com>
2026-01-07 19:58:56 -0500

    feat: play individual round

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ const getHumanChoice = () => {
     const answer = prompt("rock, paper or scissors?");
     return answer;
 }
+
+const playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        console.log("It is a draw");
+        return;
+    }
+    if (
+        (humanChoice === "paper" && computerChoice === "rock")
+        || (humanChoice === "rock" && computerChoice === "scissors")
+        || (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log(`Human wins with ${humanChoice} against ${computerChoice}`);
+    } else {
+        console.log(`Computer wins with ${computerChoice} against ${humanChoice}`);
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());

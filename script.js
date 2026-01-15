871ea7bee53436569e206a4244be5da7a40cb67c
Saten <249590842+satenshii@users.noreply.github.com>
2026-01-15 09:40:55 -0500

    feat: add score ui elements

--- script.js
+++ script.js
@@ -23,29 +23,20 @@ const checkHumanWin = (humanChoice, computerChoice) => {
         || (humanChoice === "scissors" && computerChoice === "paper");
 }
 
-const playGame = () => {
 
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    const playRound = (humanChoice, computerChoice) => {
-        humanChoice = humanChoice.toLowerCase();
-        computerChoice = computerChoice.toLowerCase();
-        if (humanChoice === computerChoice) {
-            return "It is a draw";
-        } else if (checkHumanWin(humanChoice, computerChoice)) {
-            humanScore++;
-            return `Human wins with ${humanChoice} against ${computerChoice}`;
-        } else {
-            computerScore++;
-            return `Computer wins with ${computerChoice} against ${humanChoice}`;
-        }
-    }
-
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(getHumanChoice(), getComputerChoice()));
-        console.log(`The scores are: \nHuman has ${humanScore} points \nComputer has ${computerScore} points`);
+const playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        return "It is a draw";
+    } else if (checkHumanWin(humanChoice, computerChoice)) {
+        humanScore++;
+        return `Human wins with ${humanChoice} against ${computerChoice}`;
+    } else {
+        computerScore++;
+        return `Computer wins with ${computerChoice} against ${humanChoice}`;
     }
 }
-
-playGame();
